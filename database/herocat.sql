78cc325542018fd7dc2bf6115c6b7dbfdeaec84b
Reza Irfan Wijaya <rezairfanwijaya23@gmail.com>
2022-01-25 21:41:32 +0700

    update database

--- database/herocat.sql
+++ database/herocat.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Waktu pembuatan: 21 Jan 2022 pada 04.41
+-- Waktu pembuatan: 25 Jan 2022 pada 15.40
 -- Versi server: 10.4.22-MariaDB
 -- Versi PHP: 7.4.27
 
@@ -33,8 +33,16 @@ CREATE TABLE `adopsi` (
   `no_telepon` int(11) NOT NULL,
   `alamat` text NOT NULL,
   `id_kucing` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
   `tanggal` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `adopsi`
+--
+
+INSERT INTO `adopsi` (`id_adopsi`, `pengadopsi`, `no_telepon`, `alamat`, `id_kucing`, `id_user`, `tanggal`) VALUES
+(1, 'reza', 1234, 'cilacap', 66, 2, '2022-01-25 14:32:53');
 
 -- --------------------------------------------------------
 
@@ -58,7 +66,7 @@ INSERT INTO `berita` (`id_berita`, `judul`, `konten`, `gambar`, `tanggal`) VALUE
 (3, 'Atikel Ke Tiga Diedit', '<p>isi artikel ke tiga</p>', '61e43ff7579d4.png', '2022-01-19 04:53:56'),
 (4, 'Artikel Ke Empat ', '<p>isi artikel ke empat diedit</p>', '61e588f860d02.png', '2022-01-17 15:19:27'),
 (11, 'Artikel Januari', '<p>isi artikel januari</p>', '61e79c1d3d747.png', '2022-01-19 05:05:33'),
-(13, 'Artikel Januari Minggu Pertama', '<p>Isi Artikel Januari Minggu Pertama hehe</p>', '61e9128456ab2.jpg', '2022-01-20 07:47:52');
+(13, 'Artikel Januari Minggu Pertama', '<p>Isi Artikel Januari Minggu Pertama hehe diedit</p>', '61e9128456ab2.jpg', '2022-01-25 14:21:43');
 
 -- --------------------------------------------------------
 
@@ -70,10 +78,19 @@ CREATE TABLE `donasi` (
   `id_donasi` int(11) NOT NULL,
   `nama_pendonasi` varchar(200) NOT NULL,
   `no_telpon` varchar(50) NOT NULL,
-  `id_kucing` int(11) NOT NULL,
-  `nominal` varchar(200) NOT NULL,
+  `nominal` int(11) DEFAULT NULL,
+  `rekening` int(11) NOT NULL,
+  `id_user` int(11) NOT NULL,
+  `bank` varchar(10) NOT NULL,
   `tanggal` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data untuk tabel `donasi`
+--
+
+INSERT INTO `donasi` (`id_donasi`, `nama_pendonasi`, `no_telpon`, `nominal`, `rekening`, `id_user`, `bank`, `tanggal`) VALUES
+(1, 'reza', '432534345', 90000000, 2147483647, 2, 'BNI', '2022-01-25 14:40:01');
 
 -- --------------------------------------------------------
 
@@ -94,10 +111,8 @@ CREATE TABLE `kucing` (
 --
 
 INSERT INTO `kucing` (`id_kucing`, `jenis_kucing`, `deskripsi_kucing`, `stok`, `gambar`) VALUES
-(60, 'lokal', 'Berat 5kg,umur  8 bln,kelamin jantan,jenis bulu long hair ekor kemoceng,kondisi sudah di steril dan vaksin 3x.', 1, '61e2ea97bea75.png'),
-(61, 'persia', 'persia', 1, '61e3dd289e3f5.png'),
-(62, 'lokal satu', 'lokal satu', 1, '61e3dd449f7b8.png'),
-(63, 'anggora dua', 'anggora dua', 1, '61e3dd6029397.png');
+(66, 'anggora', 'Berat 5kg,umur 8 bln,kelamin jantan,jenis bulu long hair ekor kemoceng,kondisi sudah di steril dan vaksin 3x.', 0, '61f0046502194.png'),
+(67, 'lokal di edit', 'Berat 5kg,umur 8 bln,kelamin jantan,jenis bulu long hair ekor kemoceng,kondisi sudah di steril dan vaksin 3x.', 1, '61f007548ef87.png');
 
 -- --------------------------------------------------------
 
@@ -107,25 +122,20 @@ INSERT INTO `kucing` (`id_kucing`, `jenis_kucing`, `deskripsi_kucing`, `stok`, `
 
 CREATE TABLE `user` (
   `id_user` int(11) NOT NULL,
-  `username` varchar(300) DEFAULT NULL,
+  `username` varchar(200) DEFAULT NULL,
   `password` varchar(300) DEFAULT NULL,
-  `gmail` varchar(300) DEFAULT NULL,
-  `level` varchar(20) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+  `gmail` text DEFAULT NULL,
+  `level` varchar(20) DEFAULT NULL,
+  `tanggal` timestamp NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data untuk tabel `user`
 --
 
-INSERT INTO `user` (`id_user`, `username`, `password`, `gmail`, `level`) VALUES
-(2, 'reza irfan wijaya', '$2y$10$YG.PIYmZb/7yCks1eZnAjuO3Ma7rlnx2oDUJGCu3orzb2lCNIzZ72', 'reza@gmail.com', 'user'),
-(9, 'reza', '$2y$10$pQa8.dMjk.C6cwG8SiYtMeCahAG43qXgY2gMMnpkFhdAxUFB4m.xW', 'reza@gmail.com', 'user'),
-(10, 'abdas', '$2y$10$AINqbT.jsg9adO/DMH3X6u6Nl3PBtWzYanJuLePm24SMnzZhZ15tC', '1@gmail.com', 'user'),
-(11, 'reza irfan', '$2y$10$zSGefWuFUMQgE9QbRCwMxOA06TNIWsWGlvNwOkjtpRZXWcLLWV1ti', '1@gmail.com', 'user'),
-(12, 'ifan', '$2y$10$g77YqdkyPYJTLVe9Q0BVQ.oYNdJJ/vV5PMB9rf3hWkTS3.u4xywb2', '1@gmail.com', 'user'),
-(13, 'admin', '$2y$10$dFqUBs8mQPD8E2p/pjK6PeOBJDnOzHIaYtGkOD3ko1FHEv3s/Bq5G', 'admin@gmail.com', 'admin'),
-(14, 'abc', '$2y$10$Gsmkz61.hBQQDW0/9ROiS.C/1bGt8BqZoPdNTinE71uCqBqoJR4BS', 'abc@gmail.com', 'user'),
-(15, 'reza ipang reza ipin', '$2y$10$dkblmSGEIFi.naDrnGh0YetMx/nG9vlEhq42HQ6uHwEZPq89AXwNG', 'rezaipangrezaipin@gmail.com', 'user');
+INSERT INTO `user` (`id_user`, `username`, `password`, `gmail`, `level`, `tanggal`) VALUES
+(1, 'admin', '$2y$10$X7p4uhvwxjbQrqszQXiaHOO3bEaUPn2s62p.HeDJynBDl/sQzPpIW', 'admin@gmail.com', 'admin', '2022-01-25 14:27:30'),
+(2, 'reza ', '$2y$10$VYU8t/W/5YeKpohMnxkfeuoVOBBrFJ/5y.wWzbYOnt6x4p29Pibgm', 'reza@gmail.com', 'user', '2022-01-25 14:28:53');
 
 --
 -- Indexes for dumped tables
@@ -136,7 +146,8 @@ INSERT INTO `user` (`id_user`, `username`, `password`, `gmail`, `level`) VALUES
 --
 ALTER TABLE `adopsi`
   ADD PRIMARY KEY (`id_adopsi`),
-  ADD KEY `fk_adopsi_kucing` (`id_kucing`);
+  ADD KEY `fk_adopsi_kucing` (`id_kucing`),
+  ADD KEY `fk_adopsi_user` (`id_user`);
 
 --
 -- Indeks untuk tabel `berita`
@@ -148,8 +159,7 @@ ALTER TABLE `berita`
 -- Indeks untuk tabel `donasi`
 --
 ALTER TABLE `donasi`
-  ADD PRIMARY KEY (`id_donasi`),
-  ADD KEY `fk_donasi_kucing` (`id_kucing`);
+  ADD PRIMARY KEY (`id_donasi`);
 
 --
 -- Indeks untuk tabel `kucing`
@@ -171,7 +181,7 @@ ALTER TABLE `user`
 -- AUTO_INCREMENT untuk tabel `adopsi`
 --
 ALTER TABLE `adopsi`
-  MODIFY `id_adopsi` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_adopsi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT untuk tabel `berita`
@@ -183,19 +193,19 @@ ALTER TABLE `berita`
 -- AUTO_INCREMENT untuk tabel `donasi`
 --
 ALTER TABLE `donasi`
-  MODIFY `id_donasi` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id_donasi` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT untuk tabel `kucing`
 --
 ALTER TABLE `kucing`
-  MODIFY `id_kucing` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=66;
+  MODIFY `id_kucing` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=68;
 
 --
 -- AUTO_INCREMENT untuk tabel `user`
 --
 ALTER TABLE `user`
-  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=16;
+  MODIFY `id_user` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
 
 --
 -- Ketidakleluasaan untuk tabel pelimpahan (Dumped Tables)
@@ -205,13 +215,8 @@ ALTER TABLE `user`
 -- Ketidakleluasaan untuk tabel `adopsi`
 --
 ALTER TABLE `adopsi`
-  ADD CONSTRAINT `fk_adopsi_kucing` FOREIGN KEY (`id_kucing`) REFERENCES `kucing` (`id_kucing`);
-
---
--- Ketidakleluasaan untuk tabel `donasi`
---
-ALTER TABLE `donasi`
-  ADD CONSTRAINT `fk_donasi_kucing` FOREIGN KEY (`id_kucing`) REFERENCES `kucing` (`id_kucing`);
+  ADD CONSTRAINT `fk_adopsi_kucing` FOREIGN KEY (`id_kucing`) REFERENCES `kucing` (`id_kucing`),
+  ADD CONSTRAINT `fk_adopsi_user` FOREIGN KEY (`id_user`) REFERENCES `user` (`id_user`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
